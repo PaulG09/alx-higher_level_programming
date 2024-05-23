b739625f8b6b9e56cbe0a4aef0c2ccd130999561
Paul09 <lotuds106@gmail.com>
2024-05-23 17:21:31 +0100

    Do hard things

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0
+-- Inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
 INSERT INTO first_table (id, name) VALUES (89, 'Best School');
