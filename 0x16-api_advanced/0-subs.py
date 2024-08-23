97024f6bd6608512305682c244114ebe4630613b
PaulG09 <lotuds107@gmail.com>
2024-08-23 22:46:10 +0000

    Do hard things

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit)
+ """
+ Queries the Reddit API and returns the number of subscribers for a given subreddit
+ If the subreddit is invalid, the function returns 0.
+ """
+
